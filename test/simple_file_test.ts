5aa3028e583fa0b66dac91a996b5e34c1d9bc86d
Jayne Doe <j4numbers@gmail.com>
2021-06-19 18:13:20 +0100

    feat(initial): add initial data load
    Signed-off-by: Jayne Doe <j4numbers@gmail.com>

---
+++ test/simple_file_test.ts
@@ -0,0 +1,27 @@
+import fs from 'fs';
+
+const testFunction = (introduction) => {
+  const test = `This is an introduction string: ${introduction}`;
+  console.log(test);
+};
+
+testFunction('Hello World');
+
+// This is a test async function that does things
+const asyncTestFunction = async () => fs.readdirSync(process.cwd());
+
+asyncTestFunction()
+  .then((result) => {
+    console.log(result);
+  });
+
+const testFunctionToo = (object) => {
+  object.test = 'new value';
+};
+
+const testVar = {
+  test_var: testFunctionToo,
+  async_test: asyncTestFunction,
+};
+
+console.log(testVar.length);
